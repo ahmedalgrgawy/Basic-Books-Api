61097fad31cb73295fa897e16d886b165a041e2f
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2024-07-29 20:37:41 +0300

    Update - Delete Book

--- src/controllers/book.controller.js
+++ src/controllers/book.controller.js
@@ -65,3 +65,58 @@ exports.saveBook = async (req, res) => {
         return res.status(500).send({ error: 'Failed to save book' })
     }
 }
+
+exports.updateBook = async (req, res) => {
+
+    try {
+
+        var bookId = req.body.bookId
+        var title = req.body.title;
+        var description = req.body.description;
+        var author = req.body.author;
+        var publisher = req.body.publisher;
+        var pages = req.body.pages;
+        var storeCode = req.body.storeCode;
+        var createdBy = "admin"
+        var createdOn = new Date()
+
+        if (!bookId, !title || !author || !publisher || !storeCode) {
+            return res.status(500).send({ error: "There is data missing" })
+        }
+
+        var values = [title, description, author, publisher, pages, storeCode, createdBy, createdOn, bookId]
+
+        var bookQuery = queries.queryList.UPDATE_BOOK_QUERY
+
+        await dbConnection.dbQuery(bookQuery, values);
+
+        return res.status(201).send({ message: "Book Updated" })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({ error: 'Failed to update book' })
+    }
+}
+
+exports.deleteBook = async (req, res) => {
+
+    var bookId = req.params.id
+
+    try {
+
+        if (!bookId) {
+            return res.status(500).send({ error: "Can Not Delete Empty Book" })
+        }
+
+        var deleteQuery = queries.queryList.DELETE_BOOK_QUERY
+
+        await dbConnection.dbQuery(deleteQuery, [bookId]);
+
+        return res.status(200).send({ message: "Book Deleted" })
+
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({ error: 'Failed to delete book' })
+    }
+
+}
--- src/routes/book.route.js
+++ src/routes/book.route.js
@@ -5,5 +5,7 @@ var bookController = require('../controllers/book.controller')
 router.get('/books', bookController.getBookList)
 router.get('/book-details/:id', bookController.getBookDetails)
 router.post('/book/save', bookController.saveBook)
+router.put('/book/update', bookController.updateBook)
+router.delete('/book/delete/:id', bookController.deleteBook)
 
 module.exports = router
