efc9dd88ab118400960d5de7b98852e9c443e010
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2024-08-03 18:24:18 +0300

    Adding Swagger

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv')
 const storeRoute = require('./routes/store.route');
 const bookRoute = require('./routes/book.route');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 dotenv.config()
 
@@ -22,6 +24,8 @@ app.get('/', (req, res) => {
 app.use('/api', storeRoute)
 
 app.use('/api', bookRoute)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(process.env.PORT, () => {
     console.log(`server running on port ${process.env.PORT}`)
